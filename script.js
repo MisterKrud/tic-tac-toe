eae49f109c14bf1934513a1c229e7aee5fa57fad
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-05-10 17:46:16 +1000

    Add player 1 and 2 status divs

--- script.js
+++ script.js
@@ -25,11 +25,8 @@ const Gameboard = (rows = 3, cols = 3) => {
     return boardWithCellValues;
   };
 
-  
-  return {getBoard, placeToken, renderBoard};
+  return { getBoard, placeToken, renderBoard };
 };
-
-
 
 function Cell() {
   let value = "-";
@@ -42,22 +39,17 @@ function Cell() {
   return { addToken, getValue };
 }
 
-const Player = (name, token) => {
-    return {name, token}
-}
-
-
-
+const Player = (name, token, score) => {
+  return { name, token, score };
+};
 
 const gameControl = () => {
   const gameboard = Gameboard(3, 3); //get the board
-  const player1 = Player("Player 1", "X");
-    const player2 = Player("Player 2", "0");
+  const player1 = Player("Player 1", "X", 0);
+  const player2 = Player("Player 2", "0", 0);
 
   const playerArray = [player1, player2]; // establish the players in an array
-  let activePlayer =  playerArray[0]; // active player is the first player
-
- 
+  let activePlayer = playerArray[0]; // active player is the first player
 
   //If player1 is active, swap the active player to player2
   const switchPlayers = () => {
@@ -74,94 +66,82 @@ const gameControl = () => {
     return activePlayer;
   };
 
-  const getActivePlayer = () => activePlayer
+  const getActivePlayer = () => activePlayer;
 
   const playRound = (row, col) => {
     const currentPlayer = getActivePlayer();
-    gameboard.placeToken(row, col, currentPlayer)
-  }
+    gameboard.placeToken(row, col, currentPlayer);
+    const gameWon = gameOver();
+    if (!gameWon) {
+      
+      switchPlayers()
+    } else {
+      currentPlayer.score = currentPlayer.score++
+      gameWon
+    }
+  };
 
   const gameOver = () => {
+    const board = gameboard.renderBoard();
 
-    const board = gameboard.renderBoard()
-    
     const cols = () => {
-      let columnArray = []
-      let r=0
-      for (let c=0; c< board[r].length; c++){
-        let arr = []
-      
-        
-      for (let r= 0; r<board.length; r++){
-    
-      arr.push(board[r][c])
-    
-       
-      }columnArray.push(arr)}
-     
-      return columnArray
-    }
-    
+      let columnArray = [];
+      let r = 0;
+      for (let c = 0; c < board[r].length; c++) {
+        let arr = [];
+
+        for (let r = 0; r < board.length; r++) {
+          arr.push(board[r][c]);
+        }
+        columnArray.push(arr);
+      }
+
+      return columnArray;
+    };
+
     const diagonals = () => {
-      let diagonalArray =[]
-      let mainDiagonal = []
-      let antiDiagonal = []
-     
-     
-      for (let d=0; d<board.length; d++){
-       
-       mainDiagonal.push(board[d][d]);
-       antiDiagonal.push(board[d][board.length-1-d])
-      
-    
-       
-      
+      let diagonalArray = [];
+      let mainDiagonal = [];
+      let antiDiagonal = [];
+
+      for (let d = 0; d < board.length; d++) {
+        mainDiagonal.push(board[d][d]);
+        antiDiagonal.push(board[d][board.length - 1 - d]);
       }
-      diagonalArray.push(mainDiagonal, antiDiagonal)
-      return diagonalArray
-      
-      }
-    
-      const rows = () => board
-    
-      const allLines = [...rows(), ...cols(), ...diagonals()]
-      console.log(allLines)
+      diagonalArray.push(mainDiagonal, antiDiagonal);
+      return diagonalArray;
+    };
 
-   
-      if (allLines.some( (arr) => arr.every((value) => value === player1.token || value === player2.token))) {
-        console.log(`${getActivePlayer().token} wins. Well done ${getActivePlayer().name}!`)
-      }
+    const rows = () => board;
 
-  }
+    const allLines = [...rows(), ...cols(), ...diagonals()];
+    console.log(allLines);
 
-  //    gameboard.renderBoard();//Put the board on the screen
-  //     console.log(`${activePlayer.name}'s turn. Place your '${activePlayer.token}'`)//Tell the player it's their turn
- 
+    if (
+      allLines.some((arr) => arr.every((value) => value === player1.token)) ||
+      allLines.some((arr) => arr.every((value) => value === player2.token))
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-  return {gameboard, getActivePlayer, gameOver, switchPlayers, playRound};
+  return { gameboard, getActivePlayer, gameOver, switchPlayers, playRound, player1, player2 };
 
-  //player puts token on board - selects row and column (how does the player do this?)
-
-  //If the space is free, the token goes into that cell
+  
 };
 
 // const gameRound1 = (() => {
-    
+
 //    const control = gameControl();
 //    const screen = screenControl();
 //    screen.renderDivs();
 //    screen.playMessage();
 //    screen.clickListener();
 //    control.gameOver();
-   
-   
-
-   
-  
-   
 
 //   })();
-
 
 // const gameRound2 = () => {
 //     const control = gameControl();
@@ -172,48 +152,74 @@ const gameControl = () => {
 // }
 
 const screenControl = (() => {
-const control = gameControl();
-const boardDiv = document.querySelector(".board")
-const messageScreen = document.getElementById("messages");
-const playMessage = () => `${control.getActivePlayer().name}'s turn. Place your ${control.getActivePlayer().token}.`
-  messageScreen.textContent= playMessage();
-const board = control.gameboard
+  const control = gameControl();
+  const boardDiv = document.querySelector(".board");
+  const messageScreen = document.getElementById("messages");
+  const playerOneDetails = document.getElementById("player-one");
+  const playerTwoDetails = document.getElementById("player-two");
+ 
+  playerOneDetails.innerHTML = `<p>${control.player1.name}: '${control.player1.token}'</p><p>Score: ${control.player1.score}</p>`
+  playerTwoDetails.innerHTML = `<p>${control.player2.name}: '${control.player2.token}'</p><p>Score: ${control.player2.score}</p>`;
 
-  
-const renderDivs = (() => {
-  
-for (let i = 0; i<board.getBoard().length; i++){
-  let divRow = document.createElement("div")
-  divRow.setAttribute("class","row")
-  boardDiv.appendChild(divRow)
-  for (let j = 0;j<board.getBoard()[i].length;j++){
 
+  const playMessage = () =>
+    `${control.getActivePlayer().name}'s turn. Place your ${
+      control.getActivePlayer().token
+    }.`;
+  const winningMessage = () =>
+    `${control.getActivePlayer().token} wins. Well done ${
+      control.getActivePlayer().name
+    }! <br/> Click here to play again.`;
+  messageScreen.textContent = playMessage();
+  const board = control.gameboard;
+
+  const renderDivs = (() => {
     
-    let divCell = document.createElement("div");
-    divCell.setAttribute("class","cell")
-    divCell.setAttribute(`id`, `c${i}${j}`)
-    divCell.setAttribute('row', `${i}`)
-    divCell.setAttribute(`col`, `${j}`)
-  divRow.appendChild(divCell);
+    for (let i = 0; i < board.getBoard().length; i++) {
+      let divRow = document.createElement("div");
+      divRow.setAttribute("class", "row");
+      boardDiv.appendChild(divRow);
+      for (let j = 0; j < board.getBoard()[i].length; j++) {
+        let divCell = document.createElement("div");
+        divCell.setAttribute("class", "cell");
+        divCell.setAttribute(`id`, `c${i}${j}`);
+        divCell.setAttribute("row", `${i}`);
+        divCell.setAttribute(`col`, `${j}`);
+        divRow.appendChild(divCell);
+      }
+    }})()
+ 
+
+    const clickListener = (() => {
+      const gridCells = document.querySelectorAll(".cell");
+      gridCells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+          const row = cell.getAttribute("row");
+          const col = cell.getAttribute("col")
+          const currentPlayer = control.getActivePlayer()
+          control.playRound(row, col);
+          const gameIsWon = control.gameOver()
+          cell.textContent = `${currentPlayer.token}`;
+          // control.switchPlayers();
+          console.log(`Active player is ${control.getActivePlayer().name}`);
+
+          if (gameIsWon) {
+            
+            // control.switchPlayers();
+
+            messageScreen.innerHTML = winningMessage();
+
+            messageScreen.addEventListener("click", () => {
+             
+              location.reload();
+            });
+          } else {
+            messageScreen.textContent = playMessage();
+          }
+        });
+      });
+    })();
   
-}
-}
+  return { control, boardDiv, board, renderDivs};
 
-const clickListener = (() => {
-const gridCells = document.querySelectorAll(".cell")
- gridCells.forEach((cell) =>{
-  cell.addEventListener("click", () => {
-    control.playRound(cell.getAttribute("row"), cell.getAttribute("col"))
-    cell.textContent = `${control.getActivePlayer().token}`
-    control.switchPlayers();
-    console.log(`Active player is ${control.getActivePlayer().name}`)
-    messageScreen.textContent=playMessage();
-   
-    
-  })
- })
-})();
-
-})()
-return {control, boardDiv, board, renderDivs}
-})()
+  })()
